c6a69359cac23f43b1b06aedf92a91c8e0b7e6a8
Connor <connor@ktoim.com>
2020-06-19 16:11:15 +0100

    remove api prefix from password reset notification

--- src/Notifications/ResetPasswordNotification.php
+++ src/Notifications/ResetPasswordNotification.php
@@ -29,7 +29,7 @@ class ResetPasswordNotification extends Notification implements ShouldQueue
         return (new MailMessage())->subject('Reset your password')
             ->greeting('Hi ' . $notifiable->full_name)
             ->line('It seems you have sent a request to reset your password, if this was you click on the button below to choose a new one.')
-            ->action('Reset Password', config('app.url') . config('jwt-auth.prefix') . '/reset-password?token=' . $this->token . '&email=' . $notifiable->email)
+            ->action('Reset Password', config('app.url') . '/reset-password?token=' . $this->token . '&email=' . $notifiable->email)
             ->line('If you didn\'t mean to reset your password or didn\'t request a reset, then you can safely ignore this email; your password will remain the same.');
     }
 
