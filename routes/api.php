b74770a878de1ae2d40b06302479333699c4c679
Ben Carter <bencarter78@hotmail.com>
2020-08-17 22:08:40 +0100

    Adding route name to password reset

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', 'AuthController@login');
-Route::post('/forgot-password', 'PasswordResetController@sendResetEmail');
+Route::post('/forgot-password', 'PasswordResetController@sendResetEmail')->name('password.reset');
 Route::post('/verify-token', 'PasswordResetController@verifyPasswordReset');
 Route::post('/reset-password', 'PasswordResetController@resetPassword');
